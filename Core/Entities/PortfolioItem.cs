4559093ebccbe0160d4f0beb3cea374afb8ab1ac
M.Moaty <M.Moaty@DESKTOP-0L7N19L>
2021-01-12 19:24:20 +0200

    Add project files.

---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.OData.Edm;
+using System;
+
+namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title  { get; set; }
+        public DateTime  Date { get; set; }
+        public string location  { get; set; }
+    }
+}
---
+++ Web/Controllers/AccountsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult Login ()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+
+    }
+}
